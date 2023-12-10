1d804daa713644ce1b67b888cd744fd6c1124daf
dy102 <ekagnlek2@gmail.com>
2023-12-11 05:22:01 +0900

    feat: 메인 메서드

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,8 +1,14 @@
 package bridge;
 
+import bridge.controller.GameController;
+import bridge.view.InputView;
+import bridge.view.OutputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new GameController(new BridgeRandomNumberGenerator(),
+                new InputView(),
+                new OutputView()).run();
     }
 }
